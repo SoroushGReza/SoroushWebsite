f4fe6da40f9112f29173c8d4de1e775e92081465
SoroushGReza <soroush_developer@hotmail.com>
2026-06-18 09:12:58 +0330

    Register HomePost in Django admin

--- backend/portfolio/admin.py
+++ backend/portfolio/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import HomePost
+
+
+@admin.register(HomePost)
+class HomePostAdmin(admin.ModelAdmin):
+    list_display = ("title", "is_published", "created_at", "updated_at")
+    list_filter = ("is_published", "created_at")
+    search_fields = ("title", "summary", "link_text", "link_url")
+    readonly_fields = ("created_at", "updated_at")
