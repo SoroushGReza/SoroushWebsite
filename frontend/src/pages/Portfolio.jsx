3ad77a42fd96241f1978adef1b20f7d9626a5718
SoroushGReza <soroush_developer@hotmail.com>
2026-06-18 08:50:15 +0330

    Add portfolio placeholder page

---
+++ frontend/src/pages/Portfolio.jsx
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+
+function Portfolio() {
+  return (
+    <Container className="page-section">
+      <h1>Portfolio</h1>
+      <p>Projects and GitHub links will be added here later.</p>
+    </Container>
+  );
+}
+
+export default Portfolio;
