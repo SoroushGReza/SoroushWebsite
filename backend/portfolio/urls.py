c458e7ae8e14e14787a4f50f6d6d30db918cdaf4
SoroushGReza <soroush_developer@hotmail.com>
2026-06-18 09:16:23 +0330

    Add HomePost API routes

--- backend/portfolio/urls.py
+++ backend/portfolio/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
 
-from .views import api_home
+from .views import HomePostViewSet, api_home
+
+router = SimpleRouter()
+router.register("home-posts", HomePostViewSet, basename="home-post")
 
 urlpatterns = [
     path("", api_home, name="api-home"),
+    path("", include(router.urls)),
 ]
