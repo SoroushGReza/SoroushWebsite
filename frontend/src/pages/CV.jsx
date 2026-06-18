1df5a444fe790dd2b659979e08ce846e32eeb51e
SoroushGReza <soroush_developer@hotmail.com>
2026-06-18 08:50:43 +0330

    Add CV placeholder page

---
+++ frontend/src/pages/CV.jsx
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+
+function CV() {
+  return (
+    <Container className="page-section">
+      <h1>CV</h1>
+      <p>
+        Downloadable PDF CV files controlled by admin will be added here later.
+      </p>
+    </Container>
+  );
+}
+
+export default CV;
