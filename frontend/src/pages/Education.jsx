f6ffe73a2037474a6039e29b8418876810c3d54f
SoroushGReza <soroush_developer@hotmail.com>
2026-06-18 08:51:00 +0330

    Add education placeholder page

---
+++ frontend/src/pages/Education.jsx
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+
+function Education() {
+  return (
+    <Container className="page-section">
+      <h1>Education</h1>
+      <p>Studies, courses and education details will be added here later.</p>
+    </Container>
+  );
+}
+
+export default Education;
