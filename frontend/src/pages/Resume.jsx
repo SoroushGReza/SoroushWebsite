3cc5490bb500670892abe0619053de31647d5107
SoroushGReza <soroush_developer@hotmail.com>
2026-06-18 08:50:31 +0330

    Add resume placeholder page

---
+++ frontend/src/pages/Resume.jsx
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+
+function Resume() {
+  return (
+    <Container className="page-section">
+      <h1>Resume</h1>
+      <p>
+        Skills, experience and professional summary will be added here later.
+      </p>
+    </Container>
+  );
+}
+
+export default Resume;
