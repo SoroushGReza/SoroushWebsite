c11a17b13dd1458ce8bbbe99fc83726e1b9f9538
SoroushGReza <soroush_developer@hotmail.com>
2026-06-18 08:51:35 +0330

    Add contact placeholder page

---
+++ frontend/src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+
+function Contact() {
+  return (
+    <Container className="page-section">
+      <h1>Contact</h1>
+      <p>A contact form for visitors will be added here later.</p>
+    </Container>
+  );
+}
+
+export default Contact;
