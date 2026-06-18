427d311aceac0d4e8e906c592ee9a0bc743fa314
SoroushGReza <soroush_developer@hotmail.com>
2026-06-18 09:15:28 +0330

    Add HomePost CRUD API viewset

--- backend/portfolio/views.py
+++ backend/portfolio/views.py
@@ -1,5 +1,11 @@
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
+from rest_framework.parsers import FormParser, JSONParser, MultiPartParser
 from rest_framework.response import Response
+
+from .models import HomePost
+from .permissions import IsAdminOrReadOnly
+from .serializers import HomePostSerializer
 
 
 @api_view(["GET"])
@@ -10,3 +16,17 @@ def api_home(request):
             "status": "ok",
         }
     )
+
+
+class HomePostViewSet(viewsets.ModelViewSet):
+    serializer_class = HomePostSerializer
+    permission_classes = [IsAdminOrReadOnly]
+    parser_classes = [MultiPartParser, FormParser, JSONParser]
+
+    def get_queryset(self):
+        queryset = HomePost.objects.all()
+
+        if self.request.user and self.request.user.is_staff:
+            return queryset
+
+        return queryset.filter(is_published=True)
