bd61ec02d91d68076b6de1e6dba33270a60778ac
SoroushGReza <soroush_developer@hotmail.com>
2026-06-18 08:51:16 +0330

    Add GitHub placeholder page

---
+++ frontend/src/pages/GitHub.jsx
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+
+function GitHub() {
+  return (
+    <Container className="page-section">
+      <h1>GitHub</h1>
+      <p>Public GitHub repositories will be listed here later.</p>
+    </Container>
+  );
+}
+
+export default GitHub;
