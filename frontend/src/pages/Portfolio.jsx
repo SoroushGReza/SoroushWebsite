133c67992ed852ac6ea907eaa89f6b2815599ee0
SoroushGReza <soroush_developer@hotmail.com>
2026-06-24 09:53:24 +0330

    Add animated hero to Portfolio page

--- frontend/src/pages/Portfolio.jsx
+++ frontend/src/pages/Portfolio.jsx
@@ -1,11 +1,21 @@
 import { Container } from "react-bootstrap";
+import AnimatedHero from "../components/home/AnimatedHero";
 
 function Portfolio() {
   return (
-    <Container className="page-section">
-      <h1>Portfolio</h1>
-      <p>Projects and GitHub links will be added here later.</p>
-    </Container>
+    <>
+      <AnimatedHero
+        canvasText="PORTFOLIO"
+        topLabel="My Portfolio Projects"
+        theme="cyan"
+      />
+
+      <section className="page-section">
+        <Container>
+          <p>Projects and GitHub links will be added here later.</p>
+        </Container>
+      </section>
+    </>
   );
 }
 
