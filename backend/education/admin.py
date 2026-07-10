c931aa7041d11d2f06f951581647acade643d750
SoroushGReza <soroush_developer@hotmail.com>
2026-07-10 12:41:44 +0330

    Register education items in admin

--- backend/education/admin.py
+++ backend/education/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import EducationItem
+
+
+@admin.register(EducationItem)
+class EducationItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "institution",
+        "education_type",
+        "start_date",
+        "end_date",
+        "is_current",
+        "display_order",
+        "is_featured",
+        "is_published",
+    )
+    list_filter = (
+        "education_type",
+        "is_current",
+        "is_featured",
+        "is_published",
+    )
+    list_editable = (
+        "display_order",
+        "is_featured",
+        "is_published",
+    )
+    search_fields = (
+        "title",
+        "institution",
+        "location",
+        "description",
+        "icon_class",
+    )
+    ordering = ("display_order", "-start_date", "title")
+    readonly_fields = ("created_at", "updated_at")
